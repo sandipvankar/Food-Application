7a40ea09014a7ddfa886e061cbc7358d509d626d
Dell <sandipvnkr1998@gmail.com>
2022-02-01 16:27:50 +0530

    success

--- app/src/main/java/com/example/DetailActivity.java
+++ app/src/main/java/com/example/DetailActivity.java
@@ -14,6 +14,21 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding =ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    try{
+        int image = getIntent().getIntExtra("image", 0);
+        int pr = Integer.parseInt(getIntent().getStringExtra("price"));
+        String name = getIntent().getStringExtra("name");
+        String description = getIntent().getStringExtra("description");
+
+        binding.detailimagee.setImageResource(image);
+        binding.pricedet.setText(String.format("%d", pr));
+        binding.namelbl.setText(name);
+        binding.descriptilbl.setText(description);
+
+         }
+    catch(Exception e)
+        {
+        }
 
 
     }
--- app/src/main/java/com/example/activity_food_menu/Adapters/MainAdapter.java
+++ app/src/main/java/com/example/activity_food_menu/Adapters/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.example.activity_food_menu.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.DetailActivity;
 import com.example.foodapp.Models.MainModel;
 import com.example.foodapp.R;
 
@@ -33,13 +35,24 @@ public class MainAdapter extends  RecyclerView.Adapter<MainAdapter.viewholder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull viewholder holder, int position) {
+    public void onBindViewHolder(@NonNull  viewholder holder, int position) {
         final MainModel model = list.get(position);
         holder.foodimg.setImageResource(model.getImage());
         holder.mainName.setText(model.getName());
         holder.price.setText(model.getPrice());
         holder.description.setText(model.getDescription());
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(context, DetailActivity.class);
+                intent.putExtra("image", model.getImage());
+                intent.putExtra("price", model.getPrice());
+                intent.putExtra("description", model.getDescription());
+                intent.putExtra("name", model.getName());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
